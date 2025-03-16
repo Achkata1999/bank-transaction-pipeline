f74f548604864a2c55e01d078825da90a6028962
Angel Dimov <angeldimov56@gmail.com>
2025-03-17 00:03:07 +0200

    Initialize project

---
+++ bank-pipeline/pipeline/src/main/java/bank/transactions/pipeline/PipelineApplication.java
@@ -0,0 +1,13 @@
+package bank.transactions.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineApplication.class, args);
+	}
+
+}
